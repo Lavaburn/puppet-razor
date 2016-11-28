f5f648585baacdd0ec727a38bd459df6317202ab
Colin Wood <cwood06@gmail.com>
2016-11-28 16:20:36 -0600

    Fix nil issue

--- lib/puppet/provider/razor_rest.rb
+++ lib/puppet/provider/razor_rest.rb
@@ -98,7 +98,7 @@ class Puppet::Provider::Rest < Puppet::Provider
     begin
       responseJson = JSON.parse(response)
     rescue
-      raise "Could not parse the JSON response from Razor: " + response
+      raise "Could not parse the JSON response from Razor: #{response}"
     end
 
     responseJson
