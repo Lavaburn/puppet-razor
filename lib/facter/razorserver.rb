bdbf8e0ba9a7096cf48c5d915f7d5c683b81831c
Jessykah Bird <jessykah@puppet.com>
2017-06-06 14:07:10 -0400

    Prevent fact from generating errors without client installed The fact tries to use the razor client to query the server without verifying the client is installed.  This issue is present on any box that happens to be using port 8080 in any fashion.  Wrapped it in an if statement that checks for presence of the razor client first.
    Also removed all sorts of trailing spaces.

--- lib/facter/razorserver.rb
+++ lib/facter/razorserver.rb
@@ -5,29 +5,32 @@
 
 require 'facter'
 
-if Facter.value(:kernel) == "Linux" 
+if Facter.value(:kernel) == "Linux"
   Facter.add('razorserver') do
     confine :kernel => :Linux
-  
-    begin                
-      # Check which ports are in use 
-      port8150_s = `/bin/netstat -ln | /bin/grep 8150 | /usr/bin/wc -l`
-      port8150 = port8150_s.gsub(/\s+/, "").to_i
-      
-      if port8150 > 0
-        razor = `/usr/local/bin/razor -u http://localhost:8150/api -v | /bin/grep "Razor Server version"`
-      else
-        port8080_s = `/bin/netstat -ln | /bin/grep 8080 | /usr/bin/wc -l`
-        port8080 = port8080_s.gsub(/\s+/, "").to_i
-        
-        if port8080 > 0
-          razor = `/usr/local/bin/razor -u http://localhost:8080/api -v | /bin/grep "Razor Server version"`
+
+    begin
+      # Check if razor client exists
+      if File.exist? '/usr/local/bin/razor'
+      # Check which ports are in use
+        port8150_s = `/bin/netstat -ln | /bin/grep 8150 | /usr/bin/wc -l`
+        port8150 = port8150_s.gsub(/\s+/, "").to_i
+
+        if port8150 > 0
+          razor = `/usr/local/bin/razor -u http://localhost:8150/api -v | /bin/grep "Razor Server version"`
         else
-          razor = ""
-        end          
+          port8080_s = `/bin/netstat -ln | /bin/grep 8080 | /usr/bin/wc -l`
+          port8080 = port8080_s.gsub(/\s+/, "").to_i
+
+          if port8080 > 0
+            razor = `/usr/local/bin/razor -u http://localhost:8080/api -v | /bin/grep "Razor Server version"`
+          end
+        end
+      else
+        razor = ""
       end
 
-      if razor =~ /Razor Server version: (.*)/          
+      if razor =~ /Razor Server version: (.*)/
         matchdata = razor.match(/Razor Server version: (.*)/)
         version = matchdata[1]
       else
@@ -37,14 +40,14 @@ if Facter.value(:kernel) == "Linux"
       puts "Error while fetching Razor Server version: #{errors.inspect} "
       version = nil
     end
-    
+
     # Create Facts
     razorserver_facts = {}
-      
-    if version then    
+
+    if version then
       razorserver_facts[:version] = version
     end
-    
+
     setcode { razorserver_facts }
   end
 end
